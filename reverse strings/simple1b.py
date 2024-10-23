feae2b78e9b3fe70861257889831c679f77cd4b4
Facundo Fernandez <facu.tha@gmail.com>
2024-10-23 00:34:28 -0600

    ReverseString

---
+++ reverse strings/simple1b.py
@@ -0,0 +1,13 @@
+# goal: Exercise 1: Reverse a Simple String. Ask user for input
+# Input: "hello"
+# Output: "olleh"
+
+
+def reverse_string(s):
+    return s[::-1]
+
+
+# Example usage
+user_input = input("type: ")  # grab input and name the variable user_input
+reverse_string = reverse_string(user_input)
+print(reverse_string)  # Output: "olleh"
