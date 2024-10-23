851d0e92cb1bc47e2f29e85385d48ed039c6bc1c
Facundo Fernandez <facu.tha@gmail.com>
2024-10-23 00:35:02 -0600

    ReverseString

---
+++ reverse strings/simple2a.py
@@ -0,0 +1,13 @@
+# goal: Exercise 2: Reverse a String with Spaces
+# Input: "h e l l o"
+# Output: "o l l e h"
+
+
+def reverse_string_with_spaces(s):
+    words = s.split(" ")
+    reversed_words = words[::-1]
+    return " ".join(reversed_words)
+
+
+# Example usage
+print(reverse_string_with_spaces("h e l l o"))  # Output: "o l l e h"
