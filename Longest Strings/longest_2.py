fd01d2ea4a3031efd64f8c948a4c85c6c4ae3b39
Facundo Fernandez <facu.tha@gmail.com>
2024-10-23 00:41:38 -0600

    LongestString

--- Longest Strings/longest_2.py
+++ Longest Strings/longest_2.py
@@ -1,11 +1,11 @@
 # Method 2: Using the max() Function with key
+# max() function is used to find the maximum value in an iterable.
+# min() is the opposite of max(). The parameter key=len means that the function will determine the "maximum" based on the length (len) of each string
+
+
 def longest_string_max(strings):
-    longest = max(
-        strings, key=len
-    )  # max() function is used to find the maximum value in an iterable.
-    shortest = min(
-        strings, key=len
-    )  # min() is the opposite of max(). The parameter key=len means that the function will determine the "maximum" based on the length (len) of each string
+    longest = max(strings, key=len)
+    shortest = min(strings, key=len)
     return longest, shortest
 
 
